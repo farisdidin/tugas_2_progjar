3c1876777874031b6a3457c0b1c856e120f5cfeb
Unknown <ilham.misbahuddin15@mhs.if.its.ac.id>
2017-12-04 00:52:51 +0700

    Selesai

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
 import socket, select, sys, os, subprocess
 import ConfigParser
-import urllib2
 
 config = ConfigParser.RawConfigParser()   
 config.read('httpserver.conf')
@@ -14,27 +13,6 @@ server_socket.listen(5)
 input_socket = [server_socket]
 list_dataset = []
 
-#------
-for dirname, dirnames, filenames in os.walk('./dataset'):
-    # print path to all subdirectories first.
-    for subdirname in dirnames:
-        print(os.path.join(dirname, subdirname))
-
-    # print path to all filenames.
-    numb = 1
-    for filename in filenames:
-        print(os.path.join(dirname, filename))
-        list_dataset.append(os.path.join(dirname, filename).split("/")[2]) 
-        numb+=1
-
-    # Advanced usage:
-    # editing the 'dirnames' list will stop os.walk() from recursing into there.
-    if '.git' in dirnames:
-        # don't go into any .git directories.
-        dirnames.remove('.git')
-
-print list_dataset
-#------
 print 'Serve at %s port %s'%server_address
 try:
 	while True:
@@ -49,6 +27,7 @@ try:
 
 				if data:
 					
+					print "--------------------"
 					print data
 
 					request_header = data.split('\r\n')
@@ -61,43 +40,65 @@ try:
 						f = open('index.html','r')
 						response_data = f.read()
 						f.close()
-						
 						content_length = len(response_data)
 						response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' +str(content_length) + '\r\n\r\n'
-						print content_length
+												
 					elif request_file == '/dataset':
-						currdir = os.getcwd()
-						proc = subprocess.Popen("php '"+currdir+"/test.php'", shell=True, stdout=subprocess.PIPE)
-						response_data = proc.stdout.read()
-						content_length = len(response_data)
-						response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' +str(content_length) + '\r\n\r\n'
+						del list_dataset[:]
+						#------
+						for dirname, dirnames, filenames in os.walk('./dataset'):
+							print "Isi direktori dataset:"
+							for subdirname in dirnames:
+								print(os.path.join(dirname, subdirname))
 
-					
+							numb = 1
+							for filename in filenames:
+								print(os.path.join(dirname, filename))
+								list_dataset.append(os.path.join(dirname, filename).split("/")[2]) 
+								numb+=1
 
-					else:
+							if '.git' in dirnames:
+								dirnames.remove('.git')
+						#------
+						print list_dataset
 						for a in list_dataset:
-							if request_file == '/dataset/'+a:
-								print "bisa tuh >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-								file_download = 'dataset/'+a
-								content_length = os.path.getsize(file_download)
-								file = open(file_download, 'rb')
-								response_data = file.read()
-								file.close()
-								response_header = 'HTTP/1.1 200 OK\r\nContent-Type: multipart/form-data; charset=UTF-8\r\nContent-Length:' +str(content_length) + '\r\n\r\n'
+							if a == 'index.html':
+								print 'zzzzzzzzzzzzzz'
+								f = open('dataset/index.html','r')
+								response_data = f.read()
+								f.close()
+								content_length = len(response_data)
+								response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' +str(content_length) + '\r\n\r\n'
+								sock.sendall(response_header + response_data )
+							else:
+								currdir = os.getcwd()
+								proc = subprocess.Popen("php '"+currdir+"/test.php'", shell=True, stdout=subprocess.PIPE)
+								response_data = proc.stdout.read()
+								content_length = len(response_data)
+								response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' +str(content_length) + '\r\n\r\n'
+								
+					else:	
+						f = open('404.html','r+')
+						response_data = f.read()
+						f.close()
+						content_length = len(response_data)
+						response_header= 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' +str(content_length) + '\r\n\r\n'	
 
-									
-						# f = open('404.html','r+')
-						# response_data = f.read()
-						# f.close()
-						# content_length = len(response_data)
-						# response_header= 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' +str(content_length) + '\r\n\r\n'	
-							
+					for a in list_dataset:
+						if request_file == '/dataset/'+a:
+							print "File berhasil didownload"
+							file_download = 'dataset/'+a
+							content_length = os.path.getsize(file_download)
+							file = open(file_download, 'rb')
+							response_data = file.read()
+							file.close()
+							response_header = 'HTTP/1.1 200 OK\r\nContent-Type: multipart/form-data; charset=UTF-8\r\nContent-Length:' +str(content_length) + '\r\n\r\n'
+						
 					sock.sendall(response_header + response_data )
 					
-					print content_length
-					print response_data
-					print "--------------------"
+					print response_header
+					print "--------------------\r\n"
 
 except KeyboardInterrupt:
 	server_socket.close()
-	sys.exit(0)
+sys.exit(0)
